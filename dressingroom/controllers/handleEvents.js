a297f4e58fed4f244d8a0c173a95f0a46c9e1b93
thangplc <thangplc1412@gmail.com>
2021-07-26 20:51:14 +0700

    update

--- dressingroom/controllers/handleEvents.js
+++ dressingroom/controllers/handleEvents.js
@@ -19,44 +19,7 @@ export default function HandleEvent(listChosen, choseItems) {
         }
     }
 
-    fadeoutBtn.onclick = () => {
-        toolBar.classList.remove("active");
-        console.log(MainScreen.classList.length);
-        prevDataset = '';
-        if (MainScreen.classList.length == 2) {
-            if (MainScreen.classList[1] != "contains") {
-                MainScreen.classList.remove(MainScreen.classList[1]);
-            } else {
-                MainScreen.classList.remove(MainScreen.classList[0]);
-            }
-        };
-        setting.style.display = "none";
-        fadeoutBtn.style.display = "none";
-        toolItem.style.display = "none";
-    }
-    setting.onclick = (event) => {
-        const clothTypes = event.target.closest('.setting__item');
 
-        let clothTypesActived = getEl('.setting__item.active')[0];
-        const clothTypesEle = event.target.closest('.setting__item p');
-        if (clothTypesEle) {
-            if (clothTypesActived) {
-                clothTypesActived.classList.remove('active');
-            }
-            clothTypes.classList.add('active');
-            if (prevDataset != clothTypes.dataset.type) {
-                MainScreen.classList.add(clothTypes.dataset.type + 'es');
-                if (prevDataset) {
-                    MainScreen.classList.remove(prevDataset);
-                }
-                prevDataset = clothTypes.dataset.type + 'es';
-            }
-            EditBtn.dataset.type = clothTypes.dataset.type;
-            DelBtn.dataset.type = clothTypes.dataset.type;
-            toolItem.style.display = "block";
-
-        }
-    }
 
 
 }
